b3104f34596ab0f82c39fd0d83d522f42a6678fa
rabiulislam007 <rabiulislam9@gmail.com>
2023-08-10 02:31:50 +0600

    graphql custom type definition implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from "graphql-yoga";
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query { 
+        post: Post!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean
+    }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    post() {
+      return {
+        id: "123098",
+        title: "Chompa",
+        body: "Chompa'r dudh khaisi, nunu chusaisi, chudsi",
+        published: true,
+      };
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up!");
+});
